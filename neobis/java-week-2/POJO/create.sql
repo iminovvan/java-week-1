83e44d587c07991b03def572a2655d18feef4af7
iminovvan <105599313+iminovvan@users.noreply.github.com>
2024-05-07 18:05:05 +0500

    fixed some problems in create.sql

--- neobis/java-week-2/POJO/create.sql
+++ neobis/java-week-2/POJO/create.sql
@@ -2,26 +2,26 @@ CREATE DATABASE CoffeeShop;
 USE CoffeeShop;
 
 CREATE TABLE Customers(
-	customer_id INT,
-	order_id INT,
-	first_name VARCHAR(30),
-	second_name VARCHAR(30),
+	customer_id INT NOT NULL,
+	order_id INT NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	second_name VARCHAR(30) NOT NULL,
 	age INT,
 	phone_number VARCHAR(15)
 );
 
 CREATE TABLE Coffee(
-	coffee_id INT,
-	name_coffee VARCHAR(15),
+	coffee_id INT NOT NULL,
+	name_coffee VARCHAR(15) NOT NULL,
 	volume INT,
 	price DECIMAL(3, 2),
 	with_ice VARCHAR(5)
 );
 
 CREATE TABLE ORDERS(
-	order_id INT,
-	customer_id INT,
-	coffee_id INT,
+	order_id INT NOT NULL,
+	customer_id INT NOT NULL,
+	coffee_id INT NOT NULL,
 	name_order VARCHAR(25),
 	price DECIMAL(3, 2)
 );
