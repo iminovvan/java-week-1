605feb7d21638c1ac79c54d8646885706f13e61b
iminovvan <105599313+iminovvan@users.noreply.github.com>
2024-05-09 22:53:17 +0500

    Update create.sql: add foreign keys

--- neobis/java-week-2/POJO/create.sql
+++ neobis/java-week-2/POJO/create.sql
@@ -4,15 +4,15 @@ USE CoffeeShop;
 CREATE TABLE Customers(
 	customer_id INT NOT NULL,
 	order_id INT NOT NULL,
-	first_name VARCHAR(30) NOT NULL,
-	second_name VARCHAR(30) NOT NULL,
+	first_name VARCHAR(250) NOT NULL,
+	second_name VARCHAR(250) NOT NULL,
 	age INT,
-	phone_number VARCHAR(15)
+	phone_number VARCHAR(400)
 );
 
 CREATE TABLE Coffee(
 	coffee_id INT NOT NULL,
-	name_coffee VARCHAR(15) NOT NULL,
+	name_coffee VARCHAR(50) NOT NULL,
 	volume INT,
 	price DECIMAL(3, 2),
 	with_ice VARCHAR(5)
@@ -26,17 +26,7 @@ CREATE TABLE ORDERS(
 	price DECIMAL(3, 2)
 );
 
--- add primary keys to each table
-ALTER TABLE Customers
-ADD PRIMARY KEY (customer_id);
-
-ALTER TABLE Orders
-ADD PRIMARY KEY (order_id);
-
-ALTER TABLE Coffee
-ADD PRIMARY KEY (coffee_id);
-
--- add foreign keys to each table
+-- foreign keys to each table
 ALTER TABLE Customers
 ADD FOREIGN KEY (order_id) REFERENCES Orders(order_id);
 
