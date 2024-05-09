55e41572be0f0f2ce9ccfdf04bf9ebede4dc607b
iminovvan <105599313+iminovvan@users.noreply.github.com>
2024-05-09 15:13:28 +0500

    fixed bugs in create.sql

--- neobis/java-week-2/POJO/create.sql
+++ neobis/java-week-2/POJO/create.sql
@@ -2,7 +2,7 @@ CREATE DATABASE CoffeeShop;
 USE CoffeeShop;
 
 CREATE TABLE Customers(
-	customer_id INT NOT NULL,
+	customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(30) NOT NULL,
 	second_name VARCHAR(30) NOT NULL,
 	age INT,
@@ -10,7 +10,7 @@ CREATE TABLE Customers(
 );
 
 CREATE TABLE Coffee(
-	coffee_id INT NOT NULL,
+	coffee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name_coffee VARCHAR(15) NOT NULL,
 	volume INT,
 	price DECIMAL(3, 2),
@@ -18,7 +18,7 @@ CREATE TABLE Coffee(
 );
 
 CREATE TABLE ORDERS(
-	order_id INT NOT NULL,
+	order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	customer_id INT NOT NULL,
 	coffee_id INT NOT NULL,
 	name_order VARCHAR(25),
