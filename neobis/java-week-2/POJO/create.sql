0e7824dc8cc0c7e2007c3bbd6eabdcac2eae3b68
iminovvan <105599313+iminovvan@users.noreply.github.com>
2024-05-12 13:32:48 +0500

    Update create.sql

--- neobis/java-week-2/POJO/create.sql
+++ neobis/java-week-2/POJO/create.sql
@@ -1,47 +1,41 @@
-CREATE DATABASE CoffeeShop;
+CREATE DATABASE IF NOT EXISTS CoffeeShop;
 USE CoffeeShop;
 
 CREATE TABLE Customers(
-	customer_id INT NOT NULL,
-	order_id INT NOT NULL,
+	customer_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(250) NOT NULL,
 	second_name VARCHAR(250) NOT NULL,
 	age INT,
-	phone_number VARCHAR(400)
+	email VARCHAR(400)
 );
 
 CREATE TABLE Coffee(
-	coffee_id INT NOT NULL,
-	name_coffee VARCHAR(50) NOT NULL,
-	customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	first_name VARCHAR(250) NOT NULL,
-	second_name VARCHAR(250) NOT NULL,
-	age INT,
-	email VARCHAR(300)
-);
-
-CREATE TABLE Coffee(
-	coffee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	coffee_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name_coffee VARCHAR(30) NOT NULL,
 	volume INT,
 	price DECIMAL(3, 2),
 	with_ice VARCHAR(5)
 );
 
-CREATE TABLE ORDERS(
-	order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	customer_id INT NOT NULL,
-	coffee_id INT NOT NULL,
-	name_order VARCHAR(25),
-	price DECIMAL(3, 2)
+CREATE TABLE Orders(
+	order_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	customer_id BIGINT NOT NULL
+);
+
+CREATE TABLE OrderItem(
+	order_item_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	order_id BIGINT NOT NULL,
+    coffee_id BIGINT NOT NULL,
+    quantity INT NOT NULL
 );
 
 -- foreign keys to each table
-ALTER TABLE Customers
-ADD FOREIGN KEY (order_id) REFERENCES Orders(order_id);
 
 ALTER TABLE Orders 
-ADD FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+ADD FOREIGN KEY (customer_id) REFERENCES Customers(customer_id);
+
+ALTER TABLE OrderItem
+ADD FOREIGN KEY (order_id) REFERENCES Orders (order_id),
 ADD FOREIGN KEY (coffee_id) REFERENCES Coffee(coffee_id);
 
 SELECT * FROM Customers;
